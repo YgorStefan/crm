a8be4ae642084c5f2fda606ee0bf89d8544bdce5
Ygor Stefankowski <ygor.stefan@gmail.com>
2026-03-31 12:54:26 -0300

    atualiza schema sql

--- database/schema.sql
+++ database/schema.sql
@@ -19,7 +19,7 @@
 
 SET FOREIGN_KEY_CHECKS = 0;
 SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
-SET time_zone = '+00:00';
+SET time_zone = '-03:00';
 
 -- ============================================================
 -- TABELA: users
